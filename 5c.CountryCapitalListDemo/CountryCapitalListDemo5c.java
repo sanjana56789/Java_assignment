21ff12208d74bb11ea8c1cd01d06ab2dc30adb77
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-20 23:44:41 +0530

    Update CountryCapitalListDemo5c.java

--- 5c.CountryCapitalListDemo/CountryCapitalListDemo5c.java
+++ 5c.CountryCapitalListDemo/CountryCapitalListDemo5c.java
@@ -1,3 +1,6 @@
+// 5c. Develop a Swing program in Java to add the countries USA, India, Vietnam, Canada,Denmark, France, Great Britain, Japan, Africa, Greenland, Singapore into a JList and display the capital of the countries on console whenever the countries are selected on the list.
+
+
 package swingprg5abcd;
 
 import javax.swing.*;
