629d49a930af8e09cd45c0a5a689dfa65d02ede4
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-20 23:25:55 +0530

    Update StringOperationsa.java

--- 3a.StringOperation/StringOperationsa.java
+++ 3a.StringOperation/StringOperationsa.java
@@ -1,3 +1,10 @@
+// 3a. Develop a java program for performing various string operations with different stringhandling functions directed as follows:String Creation and Basic Operations, Length and Character Access, String Comparison, String
+// Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+// startsWith() and endsWith()
+
+
+
+
 package stringhandlingfunc3abc;
 
 import java.util.Arrays;
