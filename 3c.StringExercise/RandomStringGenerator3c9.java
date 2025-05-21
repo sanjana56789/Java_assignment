789ff6272cdd7a13bd409e2bdf83aa492287ff31
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-21 21:43:13 +0530

    Update RandomStringGenerator3c9.java

--- 3c.StringExercise/RandomStringGenerator3c9.java
+++ 3c.StringExercise/RandomStringGenerator3c9.java
@@ -1,4 +1,4 @@
-// 3c.9 Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+// 3c.9 Write a Java Program for Creating a random string of a specified length using user defined function generateRandomString()
 
 package stringhandlingfunc3abc;
 
