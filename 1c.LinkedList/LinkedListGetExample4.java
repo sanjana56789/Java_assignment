1d37b534e3583c4417584767f49dfcd6c588ba88
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-21 15:30:31 +0530

    Update LinkedListGetExample4.java

--- 1c.LinkedList/LinkedListGetExample4.java
+++ 1c.LinkedList/LinkedListGetExample4.java
@@ -1,3 +1,5 @@
+// 1c.4 Write a Java Program for Creating a random string of a specified length using user defined function generateRandomString()
+
 package linkedlist;
 
 import java.util.LinkedList;
