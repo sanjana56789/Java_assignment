98bc6d1080af3990c3ae65a2894c89e4a1c9ef69
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-21 15:26:58 +0530

    Update ColorDeleteNth5.java

--- 1b.ArrayList/ColorDeleteNth5.java
+++ 1b.ArrayList/ColorDeleteNth5.java
@@ -1,3 +1,5 @@
+// 1b.5 Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+
 package arraylist;
 
 import java.util.ArrayList;
