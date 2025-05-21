b26e0e88df0bc7bf606dd603f3864aed697a4b83
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-21 21:33:58 +0530

    Update StringReverser3b3.java

--- 3b.StringExercise/StringReverser3b3.java
+++ 3b.StringExercise/StringReverser3b3.java
@@ -1,4 +1,4 @@
-// 3b.3 Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+// 3b.3 Write a Java Program for Reversing the characters in a string using user defined function reverseString().
 
 package stringhandlingfunc3abc;
 
