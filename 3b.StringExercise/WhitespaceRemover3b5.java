83a032c69ab403aacee299ab551731772a5ebb95
Sanjana Madi <152284962+sanjana56789@users.noreply.github.com>
2025-05-21 21:35:11 +0530

    Update WhitespaceRemover3b5.java

--- 3b.StringExercise/WhitespaceRemover3b5.java
+++ 3b.StringExercise/WhitespaceRemover3b5.java
@@ -1,4 +1,4 @@
-// 3b.5 Write a Java program that swaps two elements ( first and third elements ) in a linked list ( using Collections.swap(l_list, 0, 2))
+// 3b.5 Write a Java Program for Eliminating all whitespace characters from a string using user defined function removeWhitespace()
 
 package stringhandlingfunc3abc;
 
